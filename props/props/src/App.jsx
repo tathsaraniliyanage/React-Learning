4f5ddaaaea56640b593f16f00111739b4c3a9aa8
tathsaraniliyanage <prabodhathathsarani28@gmail.com>
2024-07-04 12:01:11 +0530

    props updated

--- props/props/src/App.jsx
+++ props/props/src/App.jsx
@@ -4,12 +4,20 @@
 // <component key=value />
 
 
+
+import Student from './Student.jsx';
+
 function App() {
-    return(
-      <>
-      
-      </>
-    )
+    return (
+        <>
+            <Student name='Spongebob' age={30} isStudent={true} />
+            <Student name='Patrick' age={42} isStudent={false} />
+            <Student name='Squidward' age={50} isStudent={false} />
+            <Student name='Sandy' age={27} isStudent={true} />
+            <Student name='Larry' age={0} isStudent={false} />
+        </>
+    );
 }
 
-export default App
+export default App;
+
--- props/props/src/Student.jsx
+++ props/props/src/Student.jsx
@@ -1,10 +1,33 @@
+import PropTypes from 'prop-types';
 
-function Student(){
-    return(
+function Student(props) {
+    return (
         <>
-        
+            <div className="student">
+                <p>
+                    Name: {props.name}
+                </p>
+                <p>
+                    Age: {props.age}
+                </p>
+                <p>
+                    Student: {props.isStudent ? 'Yes' : 'No'}
+                </p>
+            </div>
         </>
-    )
+    );
 }
 
-export default Student
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+};
+
+Student.defaultProps = {
+    name: 'Guest',
+    age: 0,
+    isStudent: false,
+};
+
+export default Student;
